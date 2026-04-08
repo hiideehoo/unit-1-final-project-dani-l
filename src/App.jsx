7aff35d3cdf55360af0929f613493b85a589141a
hiideehoo <hiideeho@gmail.com>
2026-04-08 00:17:38 -0500

    all components moved to where they need to be

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,20 @@
 import { useState, useEffect } from 'react'
 import Map from '../src/assets/Map.jsx'
 import Menu from '../src/assets/Menu.jsx'
+import ItemInteraction from './assets/Player.jsx'
 import './App.css'
 
-class Item {
-  constructor(name, className, value, vis, coords) {
-    this.name = name;
-    this.className = className;
-    this.value = value;
-    this.vis = vis;
-    this.coords = coords;
-  }
-}
-
-let items = {
-  shield: new Item("shield", "dot", 300, "", [100, 100])
-}
-
-function ItemInteraction({ latitude, longitude, entity }) {
-
-  const [entityVis, setEntityVis] = useState("visible");
-  items[entity].vis = entityVis;
-  const radius = ((((longitude - items[entity].coords[0] + 20) ** 2) + ((latitude - items[entity].coords[1] + 20) ** 2)) ** 0.5);
-  const inRange = (radius < 50) && (items[entity].vis === "visible");
-
-  const keyDown = (event) => {
-    if (inRange) {
-      if (event.key === " ") {
-        setEntityVis(prev => prev === "hidden");
-          if (entity === "shield") {player.hitPoints += 4; player.inventory.push(entity);}
-          if (entity === "sword") {player.hitPoints += 4; player.inventory.push(entity);}
-      }
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('keydown', keyDown);
-
-    return (() => {
-      window.removeEventListener('keydown', keyDown);
-    })
-  })
-
-  function Inventory({entity}) {
-    if (entityVis === "visible") {
-      return (
-        <div>
-          <div className={items[entity].className} style={{ visibility: entityVis, left: `${items[entity].coords[0]}px`, top: `${items[entity].coords[1]}px` }} />
-        </div>
-      )
-    }
-  }
-  function Popup({ entity }) {
-    if (inRange) {
-      return (
-        <div>
-          <p style={{ position: "absolute", left: `${items[entity].coords[0] - 35}px`, top: `${items[entity].coords[1] + 35}px` }}>[space] collect</p>
-        </div>
-      )
-    }
-  }
-
-  return (
-    <div>
-      <Inventory entity={entity} />
-      <Popup entity={entity} />
-    </div>
-  )
-}
-
-
-
-
-
-let player = {
-  inventory: ["apple"],
-  hitPoints: 1,
-  damage: 1,
-  silver: 100,
-  Player: function ({ latitude, longitude, nameChange, colorChange }) {
-    return (
-      <div className="box" id="player" style={{ width: "20px", height: "20px", backgroundColor: colorChange, padding: "10px", margin: "10px", position: "absolute", top: latitude + "px", left: longitude + "px", fontSize: 13 - nameChange.length }}>
-        {nameChange}
-      </div>
-    )
-  }
-}
-
-
-
-
 function App() {
-
-  class Border {
-    constructor(width, height, x, y) {
-      this.width = width;
-      this.height = height;
-      this.x = x;
-      this.y = y;
-    }
-  }
-
-  const borderCollision = {
-    room1: {
-      west: new Border(10, 500, 0, 0),
-      east: new Border(10, 500, 500, 0),
-      north: new Border(500, 10, 0, 0),
-      south: new Border(500, 10, 0, 500)
-    }
-  }
 
   const [latitude, setLatitude] = useState(20);
   const [longitude, setLongitude] = useState(480);
   const [showProfile, setShowProfile] = useState("hidden");
   const [nameChange, setNameChange] = useState("");
   const [colorChange, setColorChange] = useState("cyan");
+  const [hPStatus, setHPStatus] = useState(1);
+  const [dMGStatus, setDMGStatus] = useState(1);
+  const [$Status, set$Status] = useState(100);
+  const [invStatus, setInvStatus] = useState(["apple"]);
 
   const keyDown = (event) => {
 
@@ -152,13 +52,47 @@ function App() {
     setColorChange(e.target.value);
   }
 
+
+
+    class Border {
+    constructor(width, height, x, y) {
+      this.width = width;
+      this.height = height;
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  const borderCollision = {
+    room1: {
+      west: new Border(10, 500, 0, 0),
+      east: new Border(10, 500, 500, 0),
+      north: new Border(500, 10, 0, 0),
+      south: new Border(500, 10, 0, 500)
+    }
+  }
+
+  let player = {
+  inventory: invStatus,
+  hitPoints: hPStatus,
+  damage: dMGStatus,
+  silver: $Status,
+  Player: function ({ latitude, longitude, nameChange, colorChange }) {
+    return (
+      <div className="box" id="player" style={{ width: "20px", height: "20px", backgroundColor: colorChange, padding: "10px", margin: "10px", position: "absolute", top: latitude + "px", left: longitude + "px", fontSize: 13 - nameChange.length }}>
+        {nameChange}
+      </div>
+    )
+  }
+}
+
   return (
     <>
       <div>
-        <ItemInteraction latitude={latitude} longitude={longitude} entity="shield" />
+        <ItemInteraction latitude={latitude} longitude={longitude} entity="shield" setDMGStatus={setDMGStatus} setInvStatus={setInvStatus} setHPStatus={setHPStatus}/>
         <player.Player latitude={latitude} longitude={longitude} nameChange={nameChange} colorChange={colorChange} />
         <Map latitude={latitude} longitude={longitude} borderCollision={borderCollision}/>
-        <Menu showProfile={showProfile} colorChange={colorChange} nameChange={nameChange} handleNameChange={handleNameChange} handleColorChange={handleColorChange} player={player}/>
+        <Menu showProfile={showProfile} colorChange={colorChange} nameChange={nameChange} handleNameChange={handleNameChange} handleColorChange={handleColorChange} hPStatus={hPStatus} dMGStatus={dMGStatus} $Status={$Status} invStatus={invStatus}/>
       </div>
     </>
   )
--- src/assets/Menu.jsx
+++ src/assets/Menu.jsx
@@ -1,15 +1,15 @@
-function Menu({ showProfile, nameChange, colorChange, handleNameChange, handleColorChange, player }) {
+function Menu({ showProfile, nameChange, colorChange, handleNameChange, handleColorChange, hPStatus, dMGStatus, $Status, invStatus }) {
 
-  function displayHP() {return (`${player.hitPoints}`)}
-  function displayDMG() {return (`${player.damage}`)}
-  function display$() {return (`${player.silver}`)}
+  function displayHP() {return hPStatus}
+  function displayDMG() {return dMGStatus}
+  function display$() {return $Status}
   function displayInv() {
-    return (`${player.inventory.join(', ')}`);
+    return (`${invStatus.join(', ')}`);
   }
 
   return (
 
-    <div className="box" id="profile" style={{ visibility: showProfile }} tabIndex="0">
+    <div className="box" id="profile" style={{ visibility: showProfile }}>
       <input type="text" id="nameSelect" name="nameSelect" placeholder="name" value={nameChange} onChange={handleNameChange}></input>
       <br />
       <select id="skinSelect" name="skinSelect" value={colorChange} onChange={handleColorChange}>
@@ -17,7 +17,7 @@ function Menu({ showProfile, nameChange, colorChange, handleNameChange, handleCo
         <option value="orange">Orange</option>
         <option value="cyan">Blue</option>
       </select>
-      <div className="box" id="playerSprite" style={{ width: "40px", height: "40px", backgroundColor: colorChange, padding: "10px", margin: "10px", position: "absolute", top: "20px", left: "430px", fontSize: 20 - (nameChange.length * 1.75) }}>{nameChange}</div>
+      <div className="box" id="playerSprite" style={{ width: "40px", height: "40px", backgroundColor: colorChange, padding: "10px", margin: "10px", position: "absolute", top: "20px", left: "430px", fontSize: 20 - (nameChange.length * 1.5) }}>{nameChange}</div>
       <br></br>
       <br></br>
       <div style={{ color: "white", textAlign: "left" }}>
