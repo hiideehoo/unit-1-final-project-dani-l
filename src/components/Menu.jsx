507d538a91b3804784dd71ee295bbaef369a9524
hiideehoo <hiideeho@gmail.com>
2026-04-13 01:00:44 -0500

    updated profile

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,42 +2,47 @@ function Menu({ showProfile, nameChange, colorChange, handleNameChange, handleCo
 
   return (
 
-    <div 
-      className="box" id="profile" 
+    <div
+      id="profile" 
       style={{ visibility: showProfile }}
     >
-
+      <br />
+      <br />
+      <br />
       <input 
         type="text" id="nameSelect" name="nameSelect" placeholder="name" 
         value={nameChange} onChange={handleNameChange}
+        style={{ position: "absolute", left: "60px" }}
       />
       <br />
-
+      <br />
       <select 
         id="skinSelect" name="skinSelect" 
         value={colorChange} onChange={handleColorChange}
+        style={{ position: "absolute", left: "120px" }}
       >
         <option value="cyan">Blue</option>
         <option value="gold">Yellow</option>
         <option value="lawngreen">Green</option>
       </select>
-      
       <div 
         className="box" id="playerSprite" 
         style={{ 
-          width: "40px", height: "40px", 
+          width: "80px", height: "80px", 
           backgroundColor: colorChange, 
-          top: "20px", left: "430px", 
-          fontSize: 20 - (nameChange.length * 1.5) 
+          top: "40px", left: "350px", 
+          fontSize: 60 - (nameChange.length * 6.5) 
         }}>
           {nameChange}
       </div>
       <br />
-
       <br />
+      <br />
+      <br />
+      <hr width="100%" size="2" />
         <div style={{ color: "white" }}>
             <h3>Stats</h3>
-            <table border="1" style={{  textAlign: "center", width: "40%" }}>
+            <table border="1" style={{  textAlign: "center", width: "80%", position: "absolute", left: "50px"}}>
               <tbody>
                 <tr>
                   <th>HP</th>
@@ -53,12 +58,14 @@ function Menu({ showProfile, nameChange, colorChange, handleNameChange, handleCo
             </table>
         </div>
       <br />
-
       <br />
+      <br />
+      <br />
+      <hr width="100%" size="2" />
       <div style={{ color: "white", textAlign: "left" }}>
         <h3>Inventory</h3>
         {invStatus.map((number, index) => (
-          <li key={index} style={{marginLeft: "15px"}}>
+          <li key={index} style={{marginLeft: "45px"}}>
             {number}
           </li>
         ))}
