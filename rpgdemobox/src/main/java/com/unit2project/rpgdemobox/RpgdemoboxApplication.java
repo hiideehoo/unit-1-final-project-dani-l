67398b30e678080e291c39d8f708bcb9535e1838
Dan <hiideeho@gmail.com>
2026-09-02 15:10:26 -0500

    added SpringBoot

---
+++ rpgdemobox/src/main/java/com/unit2project/rpgdemobox/RpgdemoboxApplication.java
@@ -0,0 +1,13 @@
+package com.unit2project.rpgdemobox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgdemoboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgdemoboxApplication.class, args);
+	}
+
+}
---
+++ rpgdemobox/src/test/java/com/unit2project/rpgdemobox/RpgdemoboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit2project.rpgdemobox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgdemoboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
