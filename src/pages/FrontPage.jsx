f4ad402f17eb787a28c29929b27dd8e8fd3392a0
Dan <hiideeho@gmail.com>
2026-09-16 17:51:50 -0500

    functional connection to server

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -2,32 +2,26 @@ import { Link } from "react-router-dom";
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
 
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
 
-// app.use(cors());
-
-function testFunction() {
-    fetch("http://localhost:8080/characters",
-        {
-            method: "POST",
-            body: JSON
-            .stringify
-            ({
-                name: "Dan",
-                color: "pink"
-            }),
-            headers: {
-                "Content-type": "application/json",
-                // "Access-Control-Allow-Origin": "http://localhost:8080/characters"
-            },
-        })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-}
 
 function FrontPage() {
+
+    const testFunction = () => {
+        fetch(`http://localhost:8080/characters`,
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    name: "Dan",
+                    color: "pink"
+                }),
+                headers: {
+                    "Content-type": "application/json",
+                },
+            })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    }
+
 // buttons to move between pages
     const begin = <Link to="/game" rel="noopener noreferrer">
         <button className="buttons" id="begin">
@@ -39,7 +33,7 @@ function FrontPage() {
             ABOUT
         </button>
     </Link>
-    const misc = <button className="buttons" id="misc" onClick={testFunction()}>
+    const misc = <button className="buttons" id="misc" onClick={testFunction}>
             TEST
         </button>
     
@@ -101,7 +95,7 @@ function FrontPage() {
             <br />
             <br />
             <br />
-             <br />
+            <br />
             <br />
             <br />
             <br />
